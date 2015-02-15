7515915ae1b0a3578ee32fc019677f7d0524e762
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-02-16 00:26:50 +0500

    Playing with structs...

--- main.rs
+++ main.rs
@@ -1,3 +1,22 @@
+struct Node {
+    next: Option<Box<Node>>,
+    data: String,    
+}
+
+use std::fmt;
+
+impl fmt::Pointer for Option<Box<Node>> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "there should be the address here :'(")
+    }
+}
+
 fn main() {
-    println!("Hello from the very first Rust program!");
+
+    let newNode: Option<Box<Node>> = Some(Box::new(Node {next: None, data: "Hello".to_string()}));
+
+    let secondNode: Node = Node {next: newNode, data: "some data".to_string()};
+
+    println!("{:p}, {}", secondNode.next, secondNode.data);
+
 }
