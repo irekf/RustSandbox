ce8eadd18e4048a9a12bdfec79a6da9f65d11823
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-02-21 23:15:55 +0500

    Progress: formatted output

--- main.rs
+++ main.rs
@@ -1,22 +1,16 @@
-struct Node {
-    next: Option<Box<Node>>,
-    data: String,    
-}
-
-use std::fmt;
-
-impl fmt::Pointer for Option<Box<Node>> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "there should be the address here :'(")
-    }
-}
 
 fn main() {
 
-    let newNode: Option<Box<Node>> = Some(Box::new(Node {next: None, data: "Hello".to_string()}));
-
-    let secondNode: Node = Node {next: newNode, data: "some data".to_string()};
-
-    println!("{:p}, {}", secondNode.next, secondNode.data);
+    // formatted output
+    println!("Hello, Rust!");
+    println!("Integer number = {}", 23);
+    println!("Float number = {}", 1.012);
+    println!("Repeat printing: {0}, {1}, {1}, {3}, {2}", 2, 1, 44, 9);
+    println!("Named arguments: {name}, {age}, {occupation}", age="24", name="Ann", occupation="Designer");
+    println!("{:08b} XOR {:08b} = {:08b}", 3i8, 32i8, 3i8 ^ 34i8);
+    println!("Floating point number {:?}", 2.910210);
+    println!("Hex: {:08X}, {:08x}", 131342112, 131342112);
+    // TODO find out why this doesn't work:
+    //println!("{0:08X}, {0:08x}", 7661);
 
 }
