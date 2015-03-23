78c5f2551c8a71dd22a420daa4d6d83da616c22b
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-03-23 22:56:35 +0500

    Init commit

---
+++ matrix_mul.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::str::FromStr;
+use std::thread;
+
+fn main() {
+
+    let mat_path = Path::new("big_matrix_1.txt");
+
+    let mat_file = File::open(&mat_path).unwrap();
+
+    let mat_reader = BufReader::new(mat_file);
+
+    let mut mat_line_iterator = mat_reader.lines();
+
+    let mut mat_content: Vec<Vec<u32>> = Vec::new();
+
+    loop {
+        match mat_line_iterator.next() {
+            Some(result) => {
+                                let mat_line = result.unwrap();
+                                let slice_iter = mat_line.as_slice().split(',')
+                                    .map(|x| {
+                                                FromStr::from_str(x).unwrap()
+                                            });
+                                mat_content.push(slice_iter.collect());
+                            },
+                                 
+            None => break,
+        }
+    }
+}
