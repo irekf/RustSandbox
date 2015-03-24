f53f25494b6b93f45104ec2dec2f5d39bb2b8e74
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-03-24 22:50:28 +0500

    Single-thread multiplication

--- matrix_mul.rs
+++ matrix_mul.rs
@@ -1,3 +1,6 @@
+#![feature(core)]
+
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
@@ -34,10 +37,41 @@ fn load_matrix(file_path: &Path) -> Vec<Vec<u32>> {
 
 fn main() {
 
-    let matrix_1: Vec<Vec<u32>> = load_matrix(Path::new("big_matrix_1.txt"));
-    let matrix_2: Vec<Vec<u32>> = load_matrix(Path::new("big_matrix_2.txt"));
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("incorrect arguments");
+        return;
+    }
 
-    println!("{}", matrix_1[3][10]);
-    println!("{}", matrix_2[72][0]);
+    let matrix_1: Vec<Vec<u32>> = load_matrix(Path::new(&args[1]));
+    let matrix_2: Vec<Vec<u32>> = load_matrix(Path::new(&args[2]));
 
+    assert!(matrix_1[0].len() == matrix_2.len());
+
+    let mut result: Vec<Vec<u32>> = Vec::new();
+    let mul_length = matrix_2.len();
+
+    for y in 0..matrix_1.len() {
+
+        let mut result_row: Vec<u32> = Vec::new();
+        let mut entry_value: u32 = 0;
+
+        for x in 0..matrix_2[0].len() {
+
+            for i in 0..mul_length { 
+                entry_value += matrix_1[y][i] * matrix_2[i][x];
+            }
+
+            result_row.push(entry_value);
+        }
+
+        result.push(result_row);
+    }
+
+    for y in 0..result.len() {
+        for x in 0..result[0].len() {
+            print!("{},", result[y][x]);
+        }
+        print!("\n");
+    }
 }
